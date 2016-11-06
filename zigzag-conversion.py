8c0674a7ad38b651e0a07d085506efc63af40206
张帅 <shuai.z@aliyun.com>
2016-11-06 21:22:42 +0800

    fix indent

--- zigzag-conversion.py
+++ zigzag-conversion.py
@@ -4,12 +4,12 @@ class Solution:
         r = []
         for k in range(0, l, m):
             r.append(s[k])
-            for i in range(1, n-1):
-                for k in range(i, l, m):
+        for i in range(1, n-1):
+            for k in range(i, l, m):
+                r.append(s[k])
+                k += m-2*i
+                if k < l:
                     r.append(s[k])
-                    k += m-2*i
-                    if k < l:
-                        r.append(s[k])
         if n > 1:
             for k in range(n-1, l, m):
                 r.append(s[k])

fdb0d1bca1b9d7adef0fb32824592ac5017e9a8b
张帅 <shuai.z@aliyun.com>
2016-11-06 20:30:06 +0800

    6

---
+++ zigzag-conversion.py
@@ -0,0 +1,16 @@
+class Solution:
+    def convert(self, s, n):
+        l, m = len(s), n + (n-2) or 1
+        r = []
+        for k in range(0, l, m):
+            r.append(s[k])
+            for i in range(1, n-1):
+                for k in range(i, l, m):
+                    r.append(s[k])
+                    k += m-2*i
+                    if k < l:
+                        r.append(s[k])
+        if n > 1:
+            for k in range(n-1, l, m):
+                r.append(s[k])
+        return ''.join(r)
